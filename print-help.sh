c61a2dfbf05c266c08294cb1caf6ba9f3bd8097f
szantaii <szantaii@gmail.com>
2013-04-06 21:11:28 +0200

    Beauty patch.

--- print-help.sh
+++ print-help.sh
@@ -5,7 +5,7 @@ print_help()
 	local help_text="Quit: ^C"
 	
 	tput cup ${grid_height} \
-		$((term_width - ${#help_text})) >> ${buffer}
+		$(($((left_padding_width + grid_width)) - ${#help_text})) >> ${buffer}
 	
 	printf "${help_text}" >> ${buffer}
 	
