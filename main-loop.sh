471a35863ce45722066722f2dada4e9a7cc80c77
szantaii <szantaii@gmail.com>
2013-04-15 10:23:56 +0200

    Added licensing information and code comments.

--- main-loop.sh
+++ main-loop.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+# bash-gorillas is a demake of QBasic GORILLAS completely rewritten
+# in Bash.
+# Copyright (C) 2013 Istvan Szantai <szantaii at sidenote dot hu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program (LICENSE).
+# If not, see <http://www.gnu.org/licenses/>.
+
 # Main loop of the game
-# 
-# Not sure if this is necessary
 main_loop()
 {
 	# Initialize variables, create screen buffer for bash-gorillas
@@ -27,6 +43,7 @@ main_loop()
 		print_wind
 		print_help
 		
+		# Loop of players throwing bananas at each other
 		while true
 		do
 			print_sun
@@ -39,6 +56,8 @@ main_loop()
 			throw_banana
 		done
 		
+		# On player hit update the score
+		# and make the winner dance
 		print_score
 		print_player_victory_dance
 	done
