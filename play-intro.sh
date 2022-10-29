cadddcbee6774e835714a4b4eeae55efe06799a0
szantaii <szantaii@gmail.com>
2022-10-29 21:46:12 +0200

    play-intro: Rework and fix play_intro function
     * Cleanup code comments
     * Rearrange intro text into an array
     * Use proper single and double quotes
     * Quote variables to avoid globbing and word splitting
     * Follow-up the changes made in read-intro-outro-continue-key.sh
     * Use proper printf format strings

--- play-intro.sh
+++ play-intro.sh
@@ -16,63 +16,76 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Plays intro: prints animated frames and intro text to the screen
+# Print animated frames and intro text to the screen
 play_intro()
 {
-    # Set $left_padding from $left_padding_width
-    # $left_padding will contain a number of
-    # $left_padding_width space characters
+    local intro_lines=()
+
+    # Set $left_padding from $left_padding_width ($left_padding will contain
+    # a number of $left_padding_width space characters)
     for ((i=0; i < left_padding_width; i++))
     do
         left_padding="${left_padding} "
     done
 
-    # Set $top_padding from $top_padding_height
-    # $top_padding will contain a number of
-    # $top_padding_height newline characters
+    # Set $top_padding from $top_padding_height ($top_padding will contain
+    # a number of $top_padding_height newline characters)
     for ((i=0; i < top_padding_height; i++))
     do
         top_padding="${top_padding}\n"
     done
 
-    # Intro text
-    local intro_text="\n\n${top_padding}${left_padding}                       \
-    B a s h   G O R I L L A S\n\n\n${left_padding}            Copyright (C) \
-Istvan Szantai \x3c\x73\x7a\x61\x6e\x74\x61\x69\x69\x40\x73\x69\x64\x65\x6e\
-\x6f\x74\x65\x2e\x68\x75\x3e 2013\n\n${left_padding}     This game \
-is a demake of QBasic GORILLAS rewritten completely in Bash.\n\n\n${left_padding}\
-             Your mission is to hit your opponent with the exploding\n\
-${left_padding}           banana by varying the angle and power of your \
-throw, taking\n${left_padding}             into account wind speed, gravity, \
-and the city skyline.\n${left_padding}*          The wind speed is show by a \
-directional arrow at the bottom\n${left_padding}            of the playing \
-field, its length relative to its strength.\n\n\n\n\n\n${left_padding}    \
-                        Press any key to continue"
+    intro_lines=(
+        ''
+        ''
+        "${top_padding}${left_padding}                           B a s h   G O R I L L A S"
+        ''
+        ''
+        "${left_padding}         Copyright (C) 2013-2022 Istvan Szantai <szantaii@gmail.com>"
+        ''
+        ''
+        "${left_padding}     This game is a demake of QBasic GORILLAS rewritten completely in Bash."
+        ''
+        ''
+        "${left_padding}             Your mission is to hit your opponent with the exploding"
+        "${left_padding}           banana by varying the angle and power of your throw, taking"
+        "${left_padding}             into account wind speed, gravity, and the city skyline."
+        "${left_padding}           The wind speed is show by a directional arrow at the bottom"
+        "${left_padding}            of the playing field, its length relative to its strength."
+        ''
+        ''
+        ''
+        ''
+        ''
+        "${left_padding}                            Press any key to continue"
+    )
 
-    # Print intro text into the screen buffer
-    printf ${intro_text} >> ${buffer}
+    # Print intro into the screen buffer
+    for intro_line in "${intro_lines[@]}"
+    do
+        printf '%s\n' "${intro_line}" >> "${buffer}"
+    done
 
     # Play animation, exit from loop when a key was pressed
     while true
     do
         print_frame_stage1
         refresh_screen
-        read_intro_outro_continue_key
+        read_intro_outro_continue_key && break
         print_frame_stage2
         refresh_screen
-        read_intro_outro_continue_key
+        read_intro_outro_continue_key && break
         print_frame_stage3
         refresh_screen
-        read_intro_outro_continue_key
+        read_intro_outro_continue_key && break
         print_frame_stage4
         refresh_screen
-        read_intro_outro_continue_key
+        read_intro_outro_continue_key && break
         print_frame_stage5
         refresh_screen
-        read_intro_outro_continue_key
+        read_intro_outro_continue_key && break
     done
 
-    # Clear and refresh screen
-    clear >> ${buffer}
+    clear >> "${buffer}"
     refresh_screen
 }
