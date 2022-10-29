52fc4c28728ea102b2fc49524dc16707fcbda661
szantaii <szantaii@gmail.com>
2022-10-29 21:57:05 +0200

    play-outro: Move the play_outro function into the main script

--- bash-gorillas.sh
+++ bash-gorillas.sh
@@ -99,7 +99,6 @@ source "${script_directory}/read-player-data.sh"
 source "${script_directory}/generate-buildings.sh"
 source "${script_directory}/read-throw-data.sh"
 source "${script_directory}/throw-banana.sh"
-source "${script_directory}/play-outro.sh"
 
 
 clear_player1()
@@ -638,6 +637,87 @@ play_intro()
     for intro_line in "${intro_lines[@]}"
     do
         printf '%s\n' "${intro_line}" >> "${buffer}"
+    done
+
+    # Play animation, exit from loop when a key was pressed
+    while true
+    do
+        print_frame_stage1
+        refresh_screen
+        read_intro_outro_continue_key && break
+        print_frame_stage2
+        refresh_screen
+        read_intro_outro_continue_key && break
+        print_frame_stage3
+        refresh_screen
+        read_intro_outro_continue_key && break
+        print_frame_stage4
+        refresh_screen
+        read_intro_outro_continue_key && break
+        print_frame_stage5
+        refresh_screen
+        read_intro_outro_continue_key && break
+    done
+
+    clear >> "${buffer}"
+    refresh_screen
+}
+
+# Print animated frames and outro text to the screen
+play_outro()
+{
+    local outro_lines=()
+
+    # Clear $top_padding and $left_padding
+    top_padding=''
+    left_padding=''
+
+    # Calculate $left_padding_width and $top_padding_height
+    left_padding_width=$(((term_width - min_term_width) / 2))
+    top_padding_height=$(((term_height - min_term_height) / 2))
+
+    # Set $left_padding from $left_padding_width ($left_padding will contain
+    # a number of $left_padding_width space characters)
+    for ((i=0; i < left_padding_width; i++))
+    do
+        left_padding="${left_padding} "
+    done
+
+    # Set $top_padding from $top_padding_height ($top_padding will contain
+    # a number of $top_padding_height newline characters)
+    for ((i=0; i < top_padding_height; i++))
+    do
+        top_padding="${top_padding}\n"
+    done
+
+    outro_lines=(
+        ''
+        ''
+        ''
+        ''
+        ''
+        ''
+        "${top_padding}${left_padding}                                   GAME OVER!"
+        ''
+        "${left_padding}                                     Score:"
+        "${left_padding}                               $(printf '%-10s' "${player1_name}")${player1_score}"
+        "${left_padding}                               $(printf '%-10s' "${player2_name}")${player2_score}"
+        ''
+        ''
+        ''
+        ''
+        ''
+        ''
+        ''
+        ''
+        ''
+        "${left_padding}                            Press any key to continue"
+    )
+
+    # Print outro text into the screen buffer
+    for outro_line in "${outro_lines[@]}"
+    do
+        printf '%s\n' "${outro_line}" >> "${buffer}"
     done
 
     # Play animation, exit from loop when a key was pressed
